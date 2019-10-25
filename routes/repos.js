c3597c0d41dab83af70d9b27bc974b3c760021aa
Jeroen Ooms <jeroenooms@gmail.com>
2019-10-25 21:14:34 +0200

    support PACKAGES.gz

--- routes/repos.js
+++ routes/repos.js
@@ -1,15 +1,15 @@
 const express = require('express');
 const createError = require('http-errors');
+const zlib = require('zlib');
 const router = express.Router();
-const pkgfields = {_id: 0, Package:1, Version:1, Depends:1, Imports:1, LinkingTo:1, 
-	Suggests:1, License:1, NeedsCompilation:1, MD5sum:1};
+const pkgfields = {_id: 0, Package: 1, Version: 1, Depends: 1, Imports: 1,
+	LinkingTo: 1, Suggests: 1, License: 1, NeedsCompilation: 1, MD5sum: 1};
 
 function write_packages(data){
 	return data.map(function(x){
 		x.MD5sum = x['MD5sum'];
 		let keys = Object.keys(x);
 		return keys.map(function(key){
-			console.log(x[key])
 			let val = x[key];
 			if(Array.isArray(val))
 				val = val.join(", ");
@@ -28,6 +28,16 @@ function packages_index(query, format, res, next){
 				res.type('text/plain');
 				res.set('Cache-Control', 'no-cache');
 				res.send(text);
+			} else if(format == 'gz') {
+				zlib.gzip(text, function(err, buffer){
+					if(err){
+						next(createError(400, "Failed to create gzip"));
+					} else {
+						res.type('application/x-gzip');
+						res.set('Cache-Control', 'no-cache');
+						res.send(buffer);
+					}
+				});
 			} else {
 				next(createError(400, "Unsupported format: " + format));
 			}
@@ -39,13 +49,13 @@ router.get('/:user/src/contrib/PACKAGES\.:ext?', function(req, res, next) {
 	packages_index({_user: req.params.user, _type: 'src'}, req.params.ext, res, next);
 });
 
-router.get('/:user/bin/windows/contrib/:rversion/PACKAGES', function(req, res, next) {
-	var query = {_user: req.params.user, _type: 'win', 'Built.R' : {$regex: '^' + req.params.rversion}};
+router.get('/:user/bin/windows/contrib/:built/PACKAGES\.:ext?', function(req, res, next) {
+	var query = {_user: req.params.user, _type: 'win', 'Built.R' : {$regex: '^' + req.params.built}};
 	packages_index(query, req.params.ext, res, next);
 });
 
-router.get('/:user/bin/macosx/el-capitan/contrib/:rversion/PACKAGES', function(req, res, next) {
-	var query = {_user: req.params.user, _type: 'mac', 'Built.R' : {$regex: '^' + req.params.rversion}};
+router.get('/:user/bin/macosx/el-capitan/contrib/:built/PACKAGES\.:ext?', function(req, res, next) {
+	var query = {_user: req.params.user, _type: 'mac', 'Built.R' : {$regex: '^' + req.params.built}};
  	packages_index(query, req.params.ext, res, next);
 });
 
