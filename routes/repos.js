e8dde88c3f121b940c47e326f68a39b7e1577c40
Jeroen Ooms <jeroenooms@gmail.com>
2019-10-28 11:03:09 +0100

    add built

--- routes/repos.js
+++ routes/repos.js
@@ -7,7 +7,7 @@ const router = express.Router();
 /* Fields included in PACKAGES indices */
 const pkgfields = {_id: 0, Package: 1, Version: 1, Depends: 1, Suggests: 1, License: 1,
 	NeedsCompilation: 1, Imports: 1, LinkingTo: 1, Enhances: 1, License_restricts_use: 1,
-	OS_type: 1, Priority: 1, License_is_FOSS: 1, Archs: 1, Path: 1, MD5sum: 1};
+	OS_type: 1, Priority: 1, License_is_FOSS: 1, Archs: 1, Path: 1, MD5sum: 1, Built: 1};
 
 /* Helpers */
 function doc_to_dcf(x){
@@ -16,6 +16,8 @@ function doc_to_dcf(x){
 		let val = x[key];
 		if(Array.isArray(val))
 			val = val.join(", ");
+		else if(key == 'Built')
+			val = "R " + Object.values(val).join("; ");
 		return key + ": " + val.replace(/\s/gi, ' ');
 	}).join("\n") + "\n\n";
 }
