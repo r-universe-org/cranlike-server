bd59f7b62ebdbd5c5fb559d6463a4c1d196acf76
Jeroen Ooms <jeroenooms@gmail.com>
2019-11-09 12:44:19 +0100

    enable cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 /* Express template stuff */
 var createError = require('http-errors');
 var express = require('express');
+var cors = require('cors')
 var path = require('path');
 var logger = require('morgan');
 
@@ -34,6 +35,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
