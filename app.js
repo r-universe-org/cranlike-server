8dc80dd48e646ce7a32e1f8d95f6eb9e6ebda2bf
Jeroen Ooms <jeroenooms@gmail.com>
2020-12-21 16:24:35 +0100

    move assets

--- app.js
+++ app.js
@@ -42,8 +42,7 @@ app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
 app.use('/', manRouter);
 app.use('/', apiRouter);
 app.use('/', reposRouter);
