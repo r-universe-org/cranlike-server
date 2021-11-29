50af7e2dedea35efd5089550e533782539492e02
Jeroen Ooms <jeroenooms@gmail.com>
2021-11-29 21:47:44 +0100

    parse maintainerinfo field

--- routes/api.js
+++ routes/api.js
@@ -198,6 +198,7 @@ function parse_builder_fields(x){
 		builder.sysdeps = rdesc.parse_dep_string(builder.sysdeps);
 	builder.vignettes = from_base64_gzip(builder.vignettes);
 	builder.commit = from_base64_gzip(builder.commit);
+	builder.maintainer = from_base64_gzip(builder.maintainer);
 	return builder;
 }
 
