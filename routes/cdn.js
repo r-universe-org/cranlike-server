f24b1785d3d85ef26e7642c8a27e5ed6626821fb
Jeroen Ooms <jeroenooms@gmail.com>
2022-01-17 23:03:40 +0100

    robustify

--- routes/cdn.js
+++ routes/cdn.js
@@ -12,26 +12,29 @@ function error_cb(status, next) {
 
 /* Proper file extension is required for CloudFlare caching */
 router.get("/cdn/:hash/:file", function(req, res, next) {
-  let hash = req.params.hash;
+  let hash = req.params.hash || "";
   let file = req.params.file || "";
+  if(hash.length != 32) //assume md5 for now
+    return next(createError(400, "Invalid hash length"));
   let cursor = bucket.find({_id: hash}, {limit:1});
-  cursor.hasNext().then(function(exists){
-    if(!exists)
-      throw "Failed to locate file in gridFS: " + hash;
-    return cursor.next().then(function(x){
-      if(file !== x.filename){
-        throw `Incorrect filename ${file} (should be be ${x.filename})`;
-      }
-      let type = x.filename.endsWith('.zip') ? 'application/zip' : 'application/x-gzip';
-      return bucket.openDownloadStream(x['_id']).pipe(
-        res.type(type).set({
-          'Content-Length': x.length,
-          'Cache-Control': 'public, max-age=31557600',
-          'Last-Modified' : x.uploadDate.toUTCString()
-        })
-      );
-    })
+  return cursor.next().then(function(x){
+    if(!x)
+      throw `Failed to locate file in gridFS: ${hash}`;
+    if(file !== x.filename)
+      throw `Incorrect filename ${file} (should be be ${x.filename})`;
+    let type = x.filename.endsWith('.zip') ? 'application/zip' : 'application/x-gzip';
+    return bucket.openDownloadStream(x['_id']).pipe(
+      res.type(type).set({
+        'Content-Length': x.length,
+        'Cache-Control': 'public, max-age=31557600',
+        'Last-Modified' : x.uploadDate.toUTCString()
+      })
+    );
   }).catch(error_cb(400, next));
+});
+
+router.get("/cdn/", function(req, res, next) {
+  next(createError(400, "Invalid request"));
 });
 
 module.exports = router;
