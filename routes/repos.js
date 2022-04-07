9c02116070e000ecf487c4cc6fcdd42f6d2f0b29
Jeroen Ooms <jeroenooms@gmail.com>
2022-04-07 15:20:12 +0200

    fix citation

--- routes/repos.js
+++ routes/repos.js
@@ -395,10 +395,10 @@ router.get('/:user/articles/:pkg/:file?', function(req, res, next){
 });
 
 /* Send 'citation.cff' file */
-router.get('/:user/cff/:pkg', function(req, res, next){
+router.get('/:user/citation/:pkg/cff', function(req, res, next){
   var pkg = req.params.pkg;
   var query = qf({_user: req.params.user, _type: 'src', Package: pkg});
-  send_extracted_file(query, 'citation.cff', req, res, next).catch(error_cb(400, next));
+  send_extracted_file(query, pkg + '/citation.cff', req, res, next).catch(error_cb(400, next));
 });
 
 router.get("/:user/stats/vignettes", function(req, res, next) {
