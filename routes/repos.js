756be239d5a1f70023c564e920d20985e06876f0
Jeroen Ooms <jeroenooms@gmail.com>
2022-08-26 11:37:29 +0200

    special case bioconductor

--- routes/repos.js
+++ routes/repos.js
@@ -79,6 +79,9 @@ function qf(x, query_by_user_or_maintainer){
     if(query_by_user_or_maintainer){
       x['_selfowned'] = true;
     }
+  } if(x._user == 'bioconductor' && query_by_user_or_maintainer){
+    delete x._user;
+    x['_builder.gitstats.bioconductor'] = {'$exists':1};
   } else if(query_by_user_or_maintainer) {
     delete x._user;
     x['$or'] = [
