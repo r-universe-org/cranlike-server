8a4df5c8515c065e6af465b7dd43afeb715e2c03
Jeroen Ooms <jeroenooms@gmail.com>
2022-11-09 10:11:09 +0100

    add some docs endpoints

--- routes/repos.js
+++ routes/repos.js
@@ -416,16 +416,31 @@ router.get('/:user/articles/:pkg/:file?', function(req, res, next){
   var pkg = req.params.pkg;
   var query = qf({_user: req.params.user, _type: 'src', Package: pkg});
   var prefix = pkg + "/inst/doc/";
-  var filename = req.params.file ? (prefix + req.params.file) : new RegExp('^' + prefix + "(.+)$");
+  var filename = req.params.file && req.params.file != 'index.html' ? (prefix + req.params.file) : new RegExp('^' + prefix + "(.+)$");
   send_extracted_file(query, filename, req, res, next).catch(error_cb(400, next));
 });
 
-/* Send individual html docs */
+/* TODO: merge docs with /articles API above! */
+router.get('/:user/docs/:pkg/doc/:file?', function(req, res, next){
+  var pkg = req.params.pkg;
+  var query = qf({_user: req.params.user, _type: 'src', Package: pkg});
+  var prefix = pkg + "/inst/doc/";
+  var filename = req.params.file && req.params.file != 'index.html' ? (prefix + req.params.file) : new RegExp('^' + prefix + "(.+)$");
+  send_extracted_file(query, filename, req, res, next).catch(error_cb(400, next));
+});
+
 router.get('/:user/docs/:pkg/NEWS:ext?', function(req, res, next){
   var pkg = req.params.pkg;
   var query = qf({_user: req.params.user, _type: 'src', Package: pkg});
   var ext = req.params.ext || '.html';
   var filename = `${pkg}/extra/NEWS${ext}`
+  send_extracted_file(query, filename, req, res, next).catch(error_cb(400, next));
+});
+
+router.get('/:user/docs/:pkg/DESCRIPTION', function(req, res, next){
+  var pkg = req.params.pkg;
+  var query = qf({_user: req.params.user, _type: 'src', Package: pkg});
+  var filename = `${pkg}/DESCRIPTION`;
   send_extracted_file(query, filename, req, res, next).catch(error_cb(400, next));
 });
 
