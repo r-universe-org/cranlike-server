e86da3ca9f16aed0944ff5b5b6458aa496118fb5
Jeroen Ooms <jeroenooms@gmail.com>
2022-12-08 13:11:44 +0100

    Rename craninfo to cranstatus

--- routes/craninfo.js
+++ routes/craninfo.js
@@ -13,8 +13,14 @@ function error_cb(status, next) {
 router.get('/:user/craninfo', function(req, res, next) {
   var packages = req.query.package.split(",").map(e => e.trim()).filter(e => e.length);
   tools.get_cran_info(packages, req.query.url).then(function(info){
-    console.log(`Length: ${info.lenght}`)
     res.send(info.length == 1 ? info[0] : info); //temporary unbox
+  }).catch(error_cb(400, next));
+});
+
+/* new global single-package api */
+router.get('/cranstatus/:package', function(req, res, next) {
+  tools.get_cran_info([req.params.package], req.query.url).then(function(info){
+    res.set('Cache-Control', 'max-age=3600, public').send(info[0]);
   }).catch(error_cb(400, next));
 });
 
