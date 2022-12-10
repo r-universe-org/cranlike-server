d57efbbcdac78673bdea27ab1b1acaecb9c7cea1
Jeroen Ooms <jeroenooms@gmail.com>
2022-12-10 16:01:43 +0100

    fetch all the things

--- src/tools.js
+++ src/tools.js
@@ -1,19 +1,27 @@
-const https = require('https');
-const axios = require('axios');
-
 /* dummy token for GH api limits */
 const token = Buffer.from('Z2hwX2IxR2RLZGN0cEZGSXZYSHUyWnlpZ0dXNmxGcHNzbTBxNGJ0Vg==', 'base64').toString();
 
-/* Promisify http */
-function http_url_exists(url){
-  return new Promise((resolve, reject) => {
-    const req = https.get(url, res => {
-      resolve(res.statusCode == 200);
-    });
-    req.on('error', reject);
-    req.end();
+function fetch_text(url, opt = {}){
+  return fetch(url, opt).then(function(response){
+    if (!response.ok) {
+      response.message = "fetch_text error: http " + response.status;
+      return Promise.reject(response);
+    }
+    return response.text();
   });
-};
+}
+
+function fetch_github(url, opt = {}){
+  opt.headers = opt.headers || {'Authorization': 'token ' + token};
+  return fetch(url, opt).then(function(response){
+    return response.json().then(function(data){
+      if (!response.ok) {
+        throw "GitHub API returned HTTP " + response.status + ": " + (data.message || data);
+      }
+      return data;
+    });
+  });
+}
 
 /* true if we either have packages in the db, or an upstream monorepo exists */
 function test_if_universe_exists(user){
@@ -26,36 +34,22 @@ function test_if_universe_exists(user){
   return packages.findOne(query).then(function(x){
     if(x) return true;
     console.log("Testing if " + url + " exists...");
-    return http_url_exists(url);
+    return fetch(url).then(response => response.ok);
   });
 }
 
 function get_registry_info(user){
   const url = 'https://api.github.com/repos/r-universe/' + user + '/actions/workflows/sync.yml/runs?per_page=1&status=completed';
-  return axios.get(url, {
-    headers: {
-      'Authorization': 'token ' + token
-    }
-  }).then(function(x){return x.data}).catch(err => {
-    const data = err.response.data;
-    throw "Failed to query GitHub: HTTP " + err.response.status + ": " + (data.message || data);
-  });
+  return fetch_github(url);
 }
 
 function get_submodule_hash(user, submodule){
   const url = `https://api.github.com/repos/r-universe/${user}/git/trees/HEAD`
-  return axios.get(url, {
-    headers: {
-      'Authorization': 'token ' + token
-    }
-  }).then(function(res){
-    var info = res.data.tree.find(file => file.path == submodule);
+  return fetch_github(url).then(function(data){
+    var info = data.tree.find(file => file.path == submodule);
     if(info && info.sha){
       return info.sha;
     }
-  }).catch(err => {
-    const data = err.response.data;
-    throw "Failed to query GitHub: HTTP " + err.response.status + ": " + (data.message || data);
   });
 }
 
@@ -64,15 +58,9 @@ function trigger_rebuild(run_path){
   if(!rebuild_token)
     throw "No rebuild_token available";
   const url = `https://api.github.com/repos/${run_path}/rerun-failed-jobs`;
-  return axios.post(url, {}, {
-    headers: {
-      'Authorization': 'token ' + rebuild_token
-    }
-  }).then(function(x){
-    return x.data;
-  }).catch(err => {
-    const data = err.response.data;
-    throw `${data.message || data} (HTTP ${err.response.status} at ${run_path})`;
+  return fetch_github(url, {
+    method: 'POST',
+    headers: {'Authorization': 'token ' + rebuild_token}
   });
 }
 
@@ -89,15 +77,6 @@ function parse_description(desc){
     date: date ? date.substring(18) : "parse failure",
     urls: [...new Set(urlarray.map(x => x.replace(/\/issues$/, "")))]
   }
-}
-
-function fetch_text(url){
-  return fetch(url).then(function(response){
-    if (!response.ok) {
-      throw new Error(`Failed to download ${url}`);
-    }
-    return response.text();
-  });
 }
 
 function get_cran_desc(package){
@@ -124,8 +103,6 @@ function get_cran_desc(package){
   });
 }
 
-//get_cran_info("curl").then(console.log)
-
 module.exports = {
   test_if_universe_exists : test_if_universe_exists,
   get_registry_info : get_registry_info,
@@ -133,3 +110,12 @@ module.exports = {
   trigger_rebuild : trigger_rebuild,
   get_cran_desc : get_cran_desc
 };
+
+/* Tests
+get_cran_desc("curl").then(console.log)
+get_cran_desc("Ohmage").then(console.log)
+get_cran_desc("doesnotexists").then(console.log)
+fetch_github('https://api.github.com/users/jeroedfsdffdsn').catch(console.log)
+get_registry_info("jeroen").then(console.log)
+get_submodule_hash("jeroen", "curl").then(console.log)
+*/
