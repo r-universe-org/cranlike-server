3d4e1c30d20783fc9ac541e1031f0a0d5be37cf5
Jeroen Ooms <jeroenooms@gmail.com>
2023-04-26 15:03:00 +0200

    Test with a single long-running R session

--- routes/webr.js
+++ routes/webr.js
@@ -3,13 +3,8 @@ const createError = require('http-errors');
 const webr = require("@r-wasm/webr");
 const router = express.Router();
 const tools = require("../src/tools.js");
-const webr_options = {
-  REnv: {
-    R_HOME: '/usr/lib/R',
-    R_DEFAULT_PACKAGES: 'NULL',
-    R_ENABLE_JIT: '0'
-  }
-};
+const session = new webr.WebR();
+session.init();
 
 function error_cb(status, next) {
   return function(err){
@@ -24,7 +19,7 @@ router.get('/:user/:package/data/:name?/:format?', function(req, res, next){
   var name = req.params.name;
   var format = req.params.format;
   var query = {'_user': user, 'Package': package, '_type': 'src'};
-  var session = new webr.WebR(webr_options);
+  var key = `${package}_${name}`
   return packages.findOne(query).then(async function(x){
     var datasets = x['_contents'] && x['_contents'].datasets || [];
     if(!name) {
@@ -44,23 +39,25 @@ router.get('/:user/:package/data/:name?/:format?', function(req, res, next){
       var rdb = new Uint8Array(buffers[0]);
       var rdx = new Uint8Array(buffers[1]);
       await session.init();
-      await session.FS.writeFile('Rdata.rdx', rdx);
-      await session.FS.writeFile('Rdata.rdb', rdb);
-      await session.evalR(`lazyLoad('Rdata', filter = function(x) x=='${name}')`);
+      await session.FS.writeFile(`${key}.rdx`, rdx);
+      await session.FS.writeFile(`${key}.rdb`, rdb);
+      await session.evalR(`lazyLoad('${key}', filter = function(x) x=='${name}')`);
       if(format == 'csv'){
-        await session.evalR(`utils::write.csv(${name}, "output.csv", row.names=FALSE)`)
-        var outbuf = await session.FS.readFile("output.csv");
+        await session.evalR(`utils::write.csv(${name}, "${key}.out", row.names=FALSE)`)
+        var outbuf = await session.FS.readFile(`${key}.out`);
         res.attachment(`${name}.csv`).send(Buffer.from(outbuf, 'binary'));
       } else if(format == 'rda') {
-        await session.evalR(`save(${name}, file="output.rda")`);
-        var outbuf = await session.FS.readFile("output.rda");
+        await session.evalR(`save(${name}, file="${key}.out")`);
+        var outbuf = await session.FS.readFile(`${key}.out`);
         res.attachment(`${name}.RData`).send(Buffer.from(outbuf, 'binary'));
       } else {
         throw "Only csv and rda format is supported for now";
       }
     }
   }).catch(error_cb(400, next)).finally(function(){
-    session.close();
+    session.evalR(`unlink(c('${key}.rdx', '${key}.rdb', '${key}.out'))`);
+    session.evalR(`rm(${key})`);
+    //session.close();
   });
 });
 
