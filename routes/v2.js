14396b0730b3987809adb3346177b7315737f5aa
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-10 16:49:02 +0200

    Keep commented out original

--- routes/v2.js
+++ routes/v2.js
@@ -203,7 +203,7 @@ router.get('/:user/:package/doc/readme', function(req, res, next){
           var el = $(el)
           var lang = el.attr('class').substring(9);
           var matcher = new RegExp(`([a-z]+::)?install_github\\(.${user}/${package}.\\)`)
-          var input = el.text().replace(matcher, `install.packages("${package}", repos = c('https://${user}.r-universe.dev', 'https://cloud.r-project.org'))`)
+          var input = el.text().replace(matcher, `# $&\ninstall.packages("${package}", repos = c('https://${user}.r-universe.dev', 'https://cloud.r-project.org'))`)
           var out = hljs.highlight(input, {language: lang}).value
           el.addClass("hljs").empty().append(out);
         } catch (e) { }
