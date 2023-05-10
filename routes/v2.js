bb40fe218c5842299c2f820c8401760f9a8fcee7
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-10 16:42:28 +0200

    Fix for single-quotes

--- routes/v2.js
+++ routes/v2.js
@@ -202,7 +202,7 @@ router.get('/:user/:package/doc/readme', function(req, res, next){
         try { //hljs errors for unsupported languages
           var el = $(el)
           var lang = el.attr('class').substring(9);
-          var matcher = new RegExp(`([a-z]+::)?install_github\\("${user}/${package}"\\)`)
+          var matcher = new RegExp(`([a-z]+::)?install_github\\(.${user}/${package}.\\)`)
           var input = el.text().replace(matcher, `install.packages("${package}", repos = c('https://${user}.r-universe.dev', 'https://cloud.r-project.org'))`)
           var out = hljs.highlight(input, {language: lang}).value
           el.addClass("hljs").empty().append(out);
