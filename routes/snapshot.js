a4e79ce6f45dd9c43c3d021726770ee9a11d5fbe
Jeroen Ooms <jeroenooms@gmail.com>
2023-05-25 20:21:50 +0200

    rename stuff

--- routes/snapshot.js
+++ routes/snapshot.js
@@ -52,16 +52,16 @@ function make_filename(doc){
   throw `Unsupported type: ${type}`;
 }
 
-function packages_snapshot(docs, archive){
+function packages_snapshot(files, archive){
   var indexes = {};
-  var promises = docs.map(function(doc){
-    var hash = doc.MD5sum;
-    var date = doc._created;
-    var filename = make_filename(doc);
+  var promises = files.map(function(x){
+    var hash = x.MD5sum;
+    var date = x._created;
+    var filename = make_filename(x);
     var dirname = path.dirname(filename);
     if(!indexes[dirname])
       indexes[dirname] = [];
-    indexes[dirname].push(doc);
+    indexes[dirname].push(x);
     return bucket.find({_id: hash}, {limit:1}).next().then(function(x){
       if (!x)
         throw `Failed to locate file in gridFS: ${hash}`;
@@ -70,13 +70,13 @@ function packages_snapshot(docs, archive){
     });
   });
   return Promise.allSettled(promises).then(async function(){
-    for (const [path, docs] of Object.entries(indexes)) {
-      var packages = docs.map(doc_to_dcf).join('');
+    for (const [path, files] of Object.entries(indexes)) {
+      var packages = files.map(doc_to_dcf).join('');
       await archive.append(packages, { name: `${path}/PACKAGES` });
       await archive.append(zlib.gzipSync(packages), { name: `${path}/PACKAGES.gz` });
     }
     archive.finalize();
-    return docs;
+    return files;
   });
 }
 
@@ -88,12 +88,12 @@ router.get('/:user/api/snapshot/:format?', function(req, res, next) {
   if(req.query.packages)
     query.Package = {'$in' : req.query.packages.split(",")};
   var cursor = packages.find(query).project(pkgfields).sort({"Package" : 1});
-  cursor.toArray().then(function(docs){
-    if(!docs.length)
+  cursor.toArray().then(function(files){
+    if(!files.length)
       throw "Query returned no packages";
     if(req.query.binaries){
       var allowed = req.query.binaries.split(",");
-      docs = docs.filter(function(doc){
+      files = files.filter(function(doc){
         var binver = doc.Built && doc.Built.R || "";
         return doc._type == 'src' || allowed.find(ver => binver.startsWith(ver));
       });
@@ -108,7 +108,7 @@ router.get('/:user/api/snapshot/:format?', function(req, res, next) {
       throw "Unsupported snapshot format: " + format;
     }
     archive.pipe(res);
-    packages_snapshot(docs, archive);
+    packages_snapshot(files, archive);
   }).catch(error_cb(400, next));
 });
 
