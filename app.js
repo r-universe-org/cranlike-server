eba178da53b899f870b4f22b55ab0cbc67681d4d
Jeroen Ooms <jeroenooms@gmail.com>
2024-05-29 10:17:39 +0200

    show errors in prod

--- app.js
+++ app.js
@@ -54,9 +54,8 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = err;
 
   // render the error page
   res.status(err.status || 500);
