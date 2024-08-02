3eaca0935c8c9ebdb888d71c7feccd9b25a81ee2
Jeroen Ooms <jeroenooms@gmail.com>
2024-08-02 13:08:22 +0200

    Fail gracefully for webR OOM See https://github.com/r-universe-org/help/issues/455

--- routes/webr.js
+++ routes/webr.js
@@ -31,16 +31,16 @@ function new_rsession(preload){
     if(age < if_older_than)
       return;
     var oldsession = session;
-    start().then(function(){
+    return start().then(function(){
       var timer = setTimeout(function(){
         console.log("Timeout: closing hung R session");
         oldsession.close();
       }, 60*1000);
-      oldsession.evalRVoid(`1+1`).finally(function(){
+      return oldsession.evalRVoid(`1+1`).finally(function(){
         clearTimeout(timer);
         console.log('Closing old R session');
-        oldsession.close();
-      });
+        return oldsession.close();
+      }).catch(e => {});
     });
   }
 
@@ -73,13 +73,24 @@ function new_pool(){
   return function(format){
     var pkg = pkgmap[format] || 'base';
     workers[pkg] = workers[pkg] || new_rsession(pkg);
-    return workers[pkg].get();
+    var session = workers[pkg].get();
+    console.log(`Selected R session with ${session.preloaded}`)
+    return session;
   }
 }
 
 var get_session = new new_pool();
 
-router.get('/:user/:package/data/:name?/:format?', function(req, res, next){
+router.get('/:user/:package/data', function(req, res, next){
+  var user =  req.params.user;
+  var package = req.params.package;
+  var query = {'_user': user, 'Package': package, '_type': 'src'};
+  return packages.findOne(query).then(function(x){
+    return res.send(x._datasets || []);
+  }).catch(err => next(createError(400, err)));
+});
+
+router.get('/:user/:package/data/:name/:format?', function(req, res, next){
   var user =  req.params.user;
   var package = req.params.package;
   var name = req.params.name;
@@ -87,7 +98,6 @@ router.get('/:user/:package/data/:name?/:format?', function(req, res, next){
   var query = {'_user': user, 'Package': package, '_type': 'src'};
   var key = `${package}_${name}`.replace(/\W+/g, "");
   var session = get_session(format);
-  console.log(`Selected R session with ${session.preloaded}`)
   var supported = ['csv', 'csv.gz', 'xlsx', 'json', 'ndjson', 'rda', 'rds'];
   return packages.findOne(query).then(async function(x){
     var etag = etagify(x['_id']);
@@ -97,69 +107,69 @@ router.get('/:user/:package/data/:name?/:format?', function(req, res, next){
     }
     var lazydata = ['yes', 'true'].includes((x['LazyData'] || "").toLowerCase());
     var datasets = x._datasets || [];
-    if(!name) {
-      return res.send(datasets);
-    } else {
-      if(!format){
-        return res.redirect(req.path.replace(/\/$/, '') + '/csv');
-      }
-      if(!supported.includes(format)){
-        throw `Unsupported format: ${format}`;
-      }
-      var ds = datasets.find(x => x.name == name);
-      if(!ds)
-        throw `No data "${name}" found in ${package}`;
-      query['_type'] = {'$in' : ['mac', 'linux']};
-      if(lazydata){
-        var files = [`Rdata.rdb`, `Rdata.rdx`];
-      } else if(ds.file) {
-        var files = [ ds.file ];
-      } else {
-        throw "Unable to extract this data";
-      }
-      var buffers = await tools.get_extracted_file(query, files.map(x => `${package}/data/${x}`));
-      for (var i = 0; i < files.length; i++) {
-        if(!buffers[i] || !buffers[i].length)
-          throw `Failed to extract ${files[i]}`;
-        var ext = files[i].match(/\..*$/)[0];
-        var inputfile = `${key}${ext}`;
-        await session.FS.writeFile(`${inputfile}`, (new Uint8Array(buffers[i])));
-      }
-      await session.evalRVoid(`datatool::convert("${inputfile}", "${name}", "${format}", "${key}.out")`,
-        {captureStreams: false});
-      var outbuf = await session.FS.readFile(`${key}.out`);
-      switch(format) {
-        case 'csv':
-        case 'csv.gz':
-        case 'rds':
-          res.attachment(`${name}.${format}`);
-          break;
-        case 'rda':
-          res.attachment(`${name}.RData`);
-          break;
-        case 'xlsx':
-          res.type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet").attachment(`${name}.xlsx`);
-          break;
-        case 'json':
-          res.type('application/json');
-          break;
-        case 'ndjson':
-          res.type("text/plain");
-          break;
-        default:
-          throw "Only csv, json, xlsx, rda format is supported";
-      }
-      return res.set('ETag', etag).set('Cache-Control', 'public, max-age=3600').send(Buffer.from(outbuf, 'binary'));
+    if(!format){
+      return res.redirect(req.path.replace(/\/$/, '') + '/csv');
     }
+    if(!supported.includes(format)){
+      throw `Unsupported format: ${format}`;
+    }
+    var ds = datasets.find(x => x.name == name);
+    if(!ds)
+      throw `No data "${name}" found in ${package}`;
+    query['_type'] = {'$in' : ['mac', 'linux']};
+    if(lazydata){
+      var files = [`Rdata.rdb`, `Rdata.rdx`];
+    } else if(ds.file) {
+      var files = [ ds.file ];
+    } else {
+      throw "Unable to extract this data";
+    }
+    var buffers = await tools.get_extracted_file(query, files.map(x => `${package}/data/${x}`));
+    for (var i = 0; i < files.length; i++) {
+      if(!buffers[i] || !buffers[i].length)
+        throw `Failed to extract ${files[i]}`;
+      var ext = files[i].match(/\..*$/)[0];
+      var inputfile = `${key}${ext}`;
+      await session.FS.writeFile(`${inputfile}`, (new Uint8Array(buffers[i])));
+    }
+    await session.evalRVoid(`datatool::convert("${inputfile}", "${name}", "${format}", "${key}.out")`,
+      {captureStreams: false});
+    var outbuf = await session.FS.readFile(`${key}.out`);
+    switch(format) {
+      case 'csv':
+      case 'csv.gz':
+      case 'rds':
+        res.attachment(`${name}.${format}`);
+        break;
+      case 'rda':
+        res.attachment(`${name}.RData`);
+        break;
+      case 'xlsx':
+        res.type("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet").attachment(`${name}.xlsx`);
+        break;
+      case 'json':
+        res.type('application/json');
+        break;
+      case 'ndjson':
+        res.type("text/plain");
+        break;
+      default:
+        throw "Only csv, json, xlsx, rda format is supported";
+    }
+    return res.set('ETag', etag).set('Cache-Control', 'public, max-age=3600').send(Buffer.from(outbuf, 'binary'));
   }).catch(function(err){
     next(createError(400, err));
     if (err instanceof webr.WebRError){
       console.log(`WEBR error at ${req.path} with message: ${err.message}`);
-      session.reset(60*1000); //restart R after error (but at most once per minute)
+      return session.reset(60*1000); //restart R after error (but at most once per minute)
     }
   }).finally(function(){
-    session.evalRVoid(`unlink('${key}.*')`);
-    session.reset(3600*1000); //restart R sessions once per hour
+    return session.evalRVoid(`unlink('${key}.*')`).then(function(e){
+      return session.reset(3600*1000); //restart R sessions once per hour
+    }).catch(function(err){
+      console.log(`WEBR seems to have crashed at ${req.path}: : ${err.message}`);
+      return session.reset(0);  //WebR has crashed (OOM); get rid of it asap.
+    });
   });
 });
 
