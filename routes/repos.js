a6e5dc1d17c90f5a1a05fb93e15b3e7d10bc6de1
Jeroen Ooms <jeroenooms@gmail.com>
2024-11-17 00:39:30 +0100

    Try again fixing unique counts

--- routes/repos.js
+++ routes/repos.js
@@ -1178,7 +1178,9 @@ router.get('/:user/stats/summary', function(req, res, next){
   function unique(k, q) {
     return packages.aggregate([
       {$match:q},
-      {$group: {_id: { $toHashedIndexKey: `$${k}`}}},
+      {$project: {x: `$${k}`}},
+      {$unwind: "$x"},
+      {$group: {_id: { $toHashedIndexKey: '$x'}}},
       {$count: "total"}
     ]).next().then(function(res){
       return {length: (res ? res.total : 0), time: (new Date()) - start};
